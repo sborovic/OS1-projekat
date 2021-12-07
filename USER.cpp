eecb4f17555ecc8140c72bcab8ecf376c1fa5c97
Stefan Borovic <sborovic@gmail.com>
2021-12-07 19:22:41 +0100

    Pass Tests [1, 6] and set up Test 7

--- USER.cpp
+++ USER.cpp
@@ -3,53 +3,142 @@
 
 
 /*
- 	 Test 5: frekventni dispatch
+ 	 Test 7: Semafori bez realizacije spavanja, deo javnog testa
 */
 
 const int n = 10;
+int count = 100;
+Semaphore mutex(1);
 
-void tick(){}
+int mutex_glb = 1;
 
-class TestThread : public Thread
-{
+void mutex_glb_wait(){
+ sloop:asm{
+	mov ax, 0
+	xchg ax, mutex_glb
+}
+ if(_AX ==0){
+	dispatch();
+	asm jmp sloop;
+ }
+}
+
+
+#define intLock mutex_glb_wait();
+#define intUnlock mutex_glb = 1;
+
+
+class BoundedBuffer {
 public:
 
-	TestThread(): Thread(){}
-	~TestThread()
-	{
-		waitToComplete();
-	}
-protected:
+	BoundedBuffer (unsigned size);
+	virtual ~BoundedBuffer ();
+	int append (char);
+	char take ();
+	int  fullCount(){return itemAvailable.val();};    // potrebno consumeru
 
-	void run();
+private:
+	unsigned Size;
+	Semaphore mutexa, mutext;
+	Semaphore spaceAvailable, itemAvailable;
+	char* buffer;
+	int head, tail;
 
 };
 
-void TestThread::run()
-{
-	for(int i=0;i<32000;i++)
-	{
-		for(int j=0;j<16;j++){
-		dispatch();
-		syncPrintf(".");
-		}
+BoundedBuffer::BoundedBuffer (unsigned size) : Size(size),
+	mutexa(1), mutext(1), spaceAvailable(size), itemAvailable(0),
+	head(0), tail(0) {
+		buffer = new char[size];
+		if (!buffer) exit(1);
 	}
+
+BoundedBuffer::~BoundedBuffer(){
+	intLock
+	delete [] buffer;
+	intUnlock
 }
 
+int BoundedBuffer::append (char d) {
+	spaceAvailable.wait(0);
+	mutexa.wait(0);
+		buffer[tail] = d;
+		tail = (tail+1)%Size;
+	mutexa.signal();
+	itemAvailable.signal();
+	return 0;
+}
+
+char BoundedBuffer::take () {
+	itemAvailable.wait(0);
+	mutext.wait(0);
+		char d = buffer[head];
+		head = (head+1)%Size;
+	mutext.signal();
+	spaceAvailable.signal();
+	return d;
+}
+
+BoundedBuffer buffer(n/2);
+
+class Producer: public Thread
+{
+public:
+	Producer(): Thread() {};
+	~Producer()
+	{
+		waitToComplete();
+	}
+
+protected:
+	void run()
+	{
+		char c;
+		while(count>0)
+		{
+			c = 65+(rand()%25);
+			buffer.append(c);
+			syncPrintf("%d. Producer %d puts '%c'\n",count,Thread::getRunningId(),c);
+			mutex.wait(0);
+			count--;
+			mutex.signal();
+		}
+	}
+};
+
+class Consumer: public Thread
+{
+public:
+	Consumer(): Thread() {};
+	~Consumer()
+	{
+		waitToComplete();
+	}
+
+protected:
+	void run()
+	{
+		char c;
+		while(count>0)
+		{
+			c = buffer.take();
+			syncPrintf("%d. Consumer %d gets '%c'\n",count,Thread::getRunningId(),c);
+			mutex.wait(0);
+			count--;
+			mutex.signal();
+		}
+	}
+};
+
+void tick(){}
 
 int userMain(int argc, char** argv)
 {
 	syncPrintf("Test starts.\n");
-	TestThread t[n];
-	int i;
-	for(i=0;i<n;i++)
-	{
-		t[i].start();
-	}
-	for(i=0;i<n;i++)
-	{
-		t[i].waitToComplete();
-	}
+	Producer p;
+	Consumer c;
+	p.start();
+	c.start();
 	syncPrintf("Test ends.\n");
 	return 0;
 }
