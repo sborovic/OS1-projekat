9b3742a38eb23cb17fe4d40a9903eead540b1009
Stefan Borovic <sborovic@gmail.com>
2021-12-19 23:52:22 +0100

    Pass Test 11

--- USER.cpp
+++ USER.cpp
@@ -9,8 +9,9 @@
 #include "list.h"
 
 
+
 /*
- 	 Test 10: Semafori sa spavanjem 2
+ 	 Test 11: Semafori sa spavanjem 3
 */
 
 int t=-1;
@@ -45,24 +46,75 @@ void TestThread::run()
 
 void tick()
 {
+	/*
 	t++;
 	if(t)
 		syncPrintf("%d\n",t);
+		*/
 }
 
-int userMain(int argc, char** argv)
+
+#include <iostream.h>
+
+Semaphore* mutex = 0;
+
+class Znak : public Thread
 {
-	syncPrintf("Test starts.\n");
-	TestThread t1(15),t2(10),t3(30);
-	t1.start();
-	t2.start();
-	t3.start();
-	s.wait(5);
-	s.signal();
-	s.wait(0);
-	s.signal();
-	syncPrintf("Test ends.\n");
+public:
+	Znak(char znak, int n) : Thread(), znak(znak), n(n) {}
+	virtual ~Znak() { waitToComplete(); }
+
+	void run()
+	{
+		for (long i = 0; i < 100000; i++)
+		{
+			if (mutex->wait(n)) {
+				cout << znak;
+				mutex->signal();
+			}
+
+		}
+
+		if (mutex->wait(n)) {
+			cout << endl << znak << " finished" << endl;
+			mutex->signal();
+		}
+	}
+
+private:
+	char znak;
+	int n;
+
+};
+
+
+int userMain(int argc, char* argv[]) {
+	mutex = new Semaphore(1);
+
+	Znak* a = new Znak('a', 10);
+	Znak* b = new Znak('b', 15);
+	Znak* c = new Znak('c', 20);
+
+	a->start();
+	b->start();
+	c->start();
+
+	delete a;
+	delete b;
+	delete c;
+
+	if (mutex->wait(1)) {
+		cout << endl << "userMain finished" << endl;
+		mutex->signal();
+	}
+
+	delete mutex;
+
+
+
 	return 0;
 }
+
+
 
 
