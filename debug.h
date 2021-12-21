7e7840165829027e83449d2c24aa2d65224f20ed
Stefan Borovic <sborovic@gmail.com>
2021-12-21 22:26:11 +0100

    Add Event files

--- debug.h
+++ debug.h
@@ -1,8 +1,9 @@
-#ifndef _UTILITY_H_
-#define _UTILITY_H_
+#ifndef _debug_h_
+#define _debug_h_
 
 #include "utility.h"
+
 #define DEBUG 0
 #define TRACE(x) do { if (DEBUG) syncPrintf x; } while (0)
 
-#endif /* _utility_h_ */
+#endif /* _debug_h_ */
